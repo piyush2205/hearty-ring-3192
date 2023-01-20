4bc9ad6b4fefcb3244f1702f68605edd1aeffc46
piyush singh <pk.rv.668@gamil.com>
2023-01-20 21:16:57 +0530

    arrowshoesmain-day2

--- arrowshoes/src/App.js
+++ arrowshoes/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './pages/navbar';
+import HomePage from './pages/homePage';
 function App() {
   return (
     <div className="App">
-     
+     <HomePage/>
     </div>
   );
 }
---
+++ arrowshoes/src/pages/homePage.jsx
@@ -0,0 +1,6 @@
+import Navbar from "./navbar"
+export  default function  HomePage(){
+    return<>
+    <Navbar/>
+    </>
+}
